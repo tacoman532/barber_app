125f77b2baad1d9dc85b45d6d7ae18eea153e1c4
tacoman532 <167257918+tacoman532@users.noreply.github.com>
2024-04-16 18:51:24 -0400

    first commit

---
+++ lib/screens/celebrity_styles_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CelebrityStylesPage extends StatefulWidget {
+  const CelebrityStylesPage({super.key});
+
+  @override
+  State<CelebrityStylesPage> createState() => _CelebrityStylesPageState();
+}
+
+class _CelebrityStylesPageState extends State<CelebrityStylesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

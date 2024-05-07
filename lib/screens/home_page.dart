6b420a2ff033e8757a44ae6e0382eb9bbb951353
tacoman532 <167257918+tacoman532@users.noreply.github.com>
2024-05-07 18:58:04 -0400

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,7 +3,6 @@ import 'package:barber_app/widgets/drawer_widget.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 
-
 class HomePage extends StatefulWidget {
   final String title;
 
@@ -17,24 +16,65 @@ class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBarWidget(title: "Home",),
+        appBar: AppBarWidget(
+          title: "Home",
+        ),
         drawer: DrawerWidget(),
-        body: Align(
-              alignment: AlignmentDirectional(-0.7, 0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 24),
-                    child: Text(
-                      'Welcome, Michael',
-                      style: GoogleFonts.oswald(
-                        fontSize: 30,
-                      ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                  child: Text(
+                    'Welcome, Michael',
+                    style: GoogleFonts.oswald(
+                      fontSize: 30,
+
                     ),
                   ),
-                ],
+                ),
               ),
-            )
-        );
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                  child: Text(
+                    'Trending Styles',
+                    style: GoogleFonts.oswald(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 150.0,
+                      child: Image(
+                        height: 100.0,
+                        image: NetworkImage('https://cdn-fnknc.nitrocdn.com/jwqHRGAzpUgGskUSHlppNQzwuXgXIKwg/assets/images/optimized/rev-bf6d941/www.fashionbeans.com/wp-content/uploads/2023/08/smartcutzbarbers_manwithnumber2andskinfadehaircut-696x445.jpg'),
+                      )
+                  ),
+                  Container(
+                      height: 150.0,
+                      child: Image(
+                        height: 100.0,
+                        image: NetworkImage('https://content.latest-hairstyles.com/wp-content/uploads/short-swept-up-fade-taper-haircut-for-men.jpg'),
+                      )
+                  )
+                ],
+              ))
+            ]
+            ),
+          ),
+        )
+    );
   }
 }
